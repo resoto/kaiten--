0b361d359022b2e96dc2a9ffd670d23aa594b5ae
resoto <makiinumkou@icloud.com>
2022-04-17 21:57:51 +0900

    update

---
+++ nodejs/kaiten.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+
+let display = (count) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            readline.cursorTo(process.stdout, 0, 0);
+            for(let i=0;(count/10)>i;i++){
+                process.stdout.write("\n");
+            }
+            for(let j=0;(count%10)>j;j++){
+                process.stdout.write("　");
+            }
+            process.stdout.write("〇〇");
+            process.stdout.write(String(count));
+            resolve();
+        }, 100);
+    });
+}
+
+
+let  main = async () => {
+    console.clear();
+    let count = 1;
+    while(1){
+        await display(count++);
+    }
+};
+
+
+main();
